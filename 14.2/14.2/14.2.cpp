e9503a0cceca5d2ca1cb81f7272658c2110f4b89
x4la1 <lebedevdenis805@gmail.com>
2025-02-28 13:15:39 +0300

    add 14.2

---
+++ 14.2/14.2/14.2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+int main()
+{
+    std::ifstream input("input.txt");
+    std::ofstream output("output.txt");
+
+    int num{};
+    input >> num;
+    if (num == 1)
+    {
+        output << 1;
+        return 0;
+    }
+
+    int sum = 0;
+    int lastI = 0;
+    std::set<int> setOfNums{};
+    for (int i = 2;;++i)
+    {
+        lastI = i;
+        sum += i;
+        if (sum > num)
+        {
+            break;
+        }
+        setOfNums.insert(i);
+    }
+    int diff = 0;
+    if (sum > num)
+    {
+        diff = lastI - (sum - num);
+    }
+
+    for (auto item: setOfNums) 
+    {
+        if (!setOfNums.count(item + diff))
+        {
+            setOfNums.insert(item + diff);
+            setOfNums.erase(item);
+            std::cout << item << " " << item + diff;
+            break;
+        }
+    }
+
+    for (auto item: setOfNums)
+    {
+        output << item << " ";
+    }
+    output << "\n";
+
+
+    return 0;
+}
+
