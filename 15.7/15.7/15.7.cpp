01106a105ee4ca3dec327e8253fef6e64d7a4318
x4la1 <lebedevdenis805@gmail.com>
2025-05-23 20:14:48 +0300

    add 8.3 need fixes

---
+++ 15.7/15.7/15.7.cpp
@@ -0,0 +1,66 @@
+﻿/*
+	1)Написать программу для игры слитки, рассмотренной в настоящем разделе.
+	Ввод из файла INPUT.TXT. В первой строке находится количество слитков N. Во второй строке идут через пробел N натуральных чисел (N ≤ 200), задающих стоимости слитков в порядке слева направо. Стоимость каждого слитка не превосходит 106.
+	Вывод в файл OUTPUT.TXT. Вывести через пробел два числа: суммы стоимостей слитков, которые может гарантировать каждый игрок при правильной игре.
+	2)Лебедев Денис ПС-23
+	3)Visual Studio C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+const uint32_t MAX_C = 91;
+const uint32_t MAX_N = 200;
+
+int main()
+{
+	std::fstream input("input.txt");
+	uint32_t N;
+	input >> N;
+	std::vector<uint32_t> bars(N + 1);
+	uint32_t bar;
+	for (auto i = 1; i <= N; ++i)
+	{
+		input >> bars[i];
+	}
+
+	std::vector<uint64_t> R(N + 1, 0);
+	R[N] = bars[N];
+	for (uint32_t K = N - 1; K >= 1; --K)
+	{
+		R[K] = R[K + 1] + bars[K];
+	}
+
+	std::vector<std::vector<uint64_t>> S(N + 1, std::vector<uint64_t>(MAX_C + 1, 0));
+	for (uint32_t M = 1; M <= MAX_C; ++M)
+	{
+		S[N][M] = bars[N];
+	}
+
+	for (uint32_t K = N - 1; K >= 1; --K)
+	{
+		for (uint32_t M = 1; M <= MAX_C; ++M)
+		{
+			if (K + 2 * M > N)
+			{
+				S[K][M] = R[K];
+			}
+			else
+			{
+				uint64_t minimum = 1e10;
+				int maxL = std::min(2 * M, N - K + 1);
+				for (int L = 1; L <= maxL; ++L)
+				{
+					uint64_t opponentScore = S[K + L][L];
+					minimum = std::min(minimum, opponentScore);
+				}
+				S[K][M] = R[K] - minimum;
+			}
+		}
+	}
+
+	std::fstream output("output.txt");
+	std::cout << S[1][1] << " " << R[1] - S[1][1] << "\n";
+}
---
+++ 8.3/8.3/8.3.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+using Table = std::vector<std::vector<uint32_t>>;
+
+int32_t FindMinInVectorOfInversions(const std::vector<int32_t>& vec, uint32_t cnt)
+{
+	for (int i = vec.size() - 1; i >= 0; --i)
+	{
+		if (vec[i] < 0)
+		{
+			continue;
+		}
+		if (vec[i] == i)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+std::pair<int32_t, int32_t> FindMinPosition(const Table& Top, const Table& Left, const Table& A)
+{
+	std::vector<std::pair<int32_t, int32_t>> minTop;
+	std::vector<std::pair<int32_t, int32_t>> minLeft;
+	std::vector<int32_t> buff;
+	int32_t temp;
+
+	//left
+	for (uint32_t i = 0; i < Left.size(); ++i)
+	{
+		for (uint32_t j = 0; j < Left[i].size(); ++j)
+		{
+			if (A[i][j] != 0)
+			{
+				continue;
+			}
+			buff.push_back(Left[i][j]);
+		}
+		temp = FindMinInVectorOfInversions(buff);
+		if (temp == -1)
+		{
+			return std::pair<int32_t, int32_t>(-1, -1);
+		}
+		minLeft.push_back({ i, temp });
+		buff.clear();
+	}
+	uint32_t cnt = 0;
+	//top
+	for (uint32_t i = 0; i < Top.size(); ++i)//N*(N + N) = N^2
+	{
+		for (uint32_t j = 0; j < Top[i].size(); ++j)
+		{
+			if (A[j][i] != 0)
+			{
+				cnt++;
+				continue;
+			}
+			buff.push_back(Top[j][i]);
+		}
+		temp = FindMinInVectorOfInversions(buff);
+		if (temp == -1)
+		{
+			return std::pair<int32_t, int32_t>(-1, -1);
+		}
+		minTop.push_back({ temp, i });
+		buff.clear();
+	}
+
+	for (uint32_t i = 0; i < minTop.size(); ++i)
+	{
+		for (uint32_t j = 0; j < minLeft.size(); ++j)
+		{
+			if (minTop[i].first == minLeft[j].first && minTop[i].second == minLeft[j].second)
+			{
+				return { minTop[i].first, minTop[i].second };
+			}
+		}
+	}
+
+
+}
+
+int main()
+{
+	std::ifstream input("INPUT.TXT");
+	std::ofstream output("OUTPUT.TXT");
+	uint32_t N;
+	input >> N;
+
+	Table Top(N, std::vector<uint32_t>(N));
+	Table Left(N, std::vector<uint32_t>(N));
+	Table A(N, std::vector<uint32_t>(N, 0));
+
+	for (uint32_t i = 0; i < N; ++i)
+	{
+		for (uint32_t j = 0; j < N; ++j)
+		{
+			input >> Top[i][j];
+		}
+	}
+
+	for (uint32_t i = 0; i < N; ++i)
+	{
+		for (uint32_t j = 0; j < N; ++j)
+		{
+			input >> Left[i][j];
+		}
+	}
+
+	for (uint32_t k = 0; k <= N * N; ++k)
+	{
+		auto position = FindMinPosition(Top, Left, A);
+		if (A[position.first][position.second] != 0)
+		{
+			output << 0 << "\n";
+			return 0;
+		}
+		A[position.first][position.second] = k;
+	}
+
+	for (uint32_t i = 0; i < N; ++i)
+	{
+		for (uint32_t j = 0; j < N; ++j)
+		{
+			output << A[i][j] << " ";
+		}
+		output << "\n";
+	}
+}
+
+
